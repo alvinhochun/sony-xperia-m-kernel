9521a76d8550daccda4920b12d9095a15eb54431
Alvin Wong <alvinhochun@gmail.com>
2013-12-13 22:19:52 +0800

    Implement kexec-hardboot
    This allows performing kexec with a full hardware reboot so that the "guest"
    kernel does not have to worry about reinitializing hardware states.
    
    I've ported the patch to this Xperia M stock kernel. The hardboot page is
    located at 0x88000000, which is exactly 1MiB below the ram_console.
    
    I've tested kexec hardboot with the following parameters:
        --mem-min=0x85000000 --mem-max=0x87ffffff
    
    Keep in mind that normally, the bootloader will append parameters (e.g. IMEI)
    to the cmdline, but doing kexec will require manually appending them.
    
    Here is a sample script that uses `sed` to do the work:
    
    - https://gist.github.com/alvinhochun/7945270
    
    Please refer to the following links for more information:
    
    - https://gist.github.com/Tasssadar/6766757
    - http://forum.xda-developers.com/showthread.php?p=46223952
    
    Original author: Mike Kasick <mike@kasick.org>
    
    Vojtech Bocek <vbocek@gmail.com>:
      I've ported it to mako and flo, it is based of my grouper port, which is
      based of Asus TF201 patche ported by Jens Andersen <jens.andersen@gmail.com>
    
      I've moved atags copying from guest to the host kernel, which means there
      is no need to patch the guest kernel, assuming the --mem-min in kexec call
      is within the first 256MB of System RAM, otherwise it will take a long time
      to load. I've also fixed /proc/atags entry, which would give the kexec-tools
      userspace binary only the first 1024 bytes of atags,
      see arch/arm/kernel/atags.c for more details.
    
      Other than that, memory-reservation code for the hardboot page and
      some assembler to do the watchdog reset on MSM chip are new for this device.
    
    ayysir <dresadd09691@gmail.com>:
      kexec: use mem_text_write_kernel_word to set reboot_code_buffer args
      in order to avoid protection faults (writes to read-only
      kernel memory) when CONFIG_STRICT_MEMORY_RWX is enabled.
    
    Conflicts:
    
    	arch/arm/mach-msm/board-8930.c

--- arch/arm/mach-msm/board-8930.c
+++ arch/arm/mach-msm/board-8930.c
@@ -1097,6 +1097,17 @@ static void __init msm8930_early_memory(void)
 
 static void __init msm8930_reserve(void)
 {
+#ifdef CONFIG_KEXEC_HARDBOOT
+	// Reserve space for hardboot page, just before the ram_console
+	//struct membank* bank = &meminfo.bank[0];
+	//phys_addr_t start = bank->start + bank->size - SZ_1M - 0x00300000;
+	phys_addr_t start = KEXEC_HB_PAGE_ADDR;
+	int ret = memblock_remove(start, SZ_1M);
+	if(!ret)
+		pr_info("Hardboot page reserved at 0x%X\n", start);
+	else
+		pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
+#endif
 	msm_reserve();
 }
 
